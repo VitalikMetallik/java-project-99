e817d4a034aa269578909d36b71df2e13e6109c1
VitalikMetallik <flash_PS4@mail.ru>
2024-10-15 01:06:14 +0300

    Fix 3

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,46 +1,61 @@
 package hexlet.code.component;
 
-import hexlet.code.dto.label.LabelCreateDTO;
-import hexlet.code.dto.taskStatus.TaskStatusCreateDTO;
+import hexlet.code.model.Label;
+import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
-import hexlet.code.repository.UserRepository;
+import hexlet.code.repository.LabelRepository;
+import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.service.CustomUserDetailsService;
-import hexlet.code.service.LabelService;
-import hexlet.code.service.TaskStatusService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
+    @Autowired
+    private final CustomUserDetailsService userService;
+    @Autowired
+    private final TaskStatusRepository taskStatusRepository;
+    @Autowired
+    private final LabelRepository labelRepository;
 
-    @Autowired
-    final UserRepository userRepository;
-    @Autowired
-    final CustomUserDetailsService customUserDetailsService;
-    @Autowired
-    final TaskStatusService taskStatusService;
-    @Autowired
-    final LabelService labelService;
+    private final Map<String, String> taskStatuses = Map.of(
+            "Draft", "draft",
+            "ToReview", "to_review",
+            "ToBeFixed", "to_be_fixed",
+            "ToPublish", "to_publish",
+            "Published", "published");
+
+    private final List<String> labels = List.of(
+            "bug",
+            "feature");
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
         var email = "hexlet@example.com";
         var userData = new User();
         userData.setEmail(email);
         userData.setPasswordDigest("qwerty");
-        customUserDetailsService.createUser(userData);
+        userService.createUser(userData);
 
-        taskStatusService.create(new TaskStatusCreateDTO("Draft", "draft"));
-        taskStatusService.create(new TaskStatusCreateDTO("To review", "to_review"));
-        taskStatusService.create(new TaskStatusCreateDTO("To be fixed", "to_be_fixed"));
-        taskStatusService.create(new TaskStatusCreateDTO("To publish", "to_publish"));
-        taskStatusService.create(new TaskStatusCreateDTO("Published", "published"));
+        for (Map.Entry<String, String> status : taskStatuses.entrySet()) {
+            var taskStatus = new TaskStatus();
+            taskStatus.setName(status.getKey());
+            taskStatus.setSlug(status.getValue());
+            taskStatusRepository.save(taskStatus);
+        }
 
-        labelService.create(new LabelCreateDTO("feature"));
-        labelService.create(new LabelCreateDTO("bug"));
+        for (String label : labels) {
+            var taskLabel = new Label();
+            taskLabel.setName(label);
+            labelRepository.save(taskLabel);
+        }
     }
+
 }
